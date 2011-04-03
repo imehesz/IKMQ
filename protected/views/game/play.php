655b9eb9aacad68591bbd8e5d7d021a3e96c05fa
Imre Mehesz <imehesz@imehesz-laptop.(none)>
2011-04-03 00:21:41 -0400

    added drag und dropp

--- protected/views/game/play.php
+++ protected/views/game/play.php
@@ -7,7 +7,8 @@ $this->breadcrumbs=array(
 	Yii::app()->clientScript->registerScript( 'gamecontrollerpath', "var gamecontrollerpath='" . Yii::app()->controller->createUrl( '/game/ajaxcheck' ) . "'", CClientScript::POS_HEAD );
 
 ?>
-<h1>PLAY</h1>
+<div><strong>GAME STATS</strong></div> 
+<div><strong>Level:</strong> <?php echo $model->level; ?> <strong>Answered </strong><span id="answered_so_far">0</span>/<?php echo $model->level; ?></div>
 
 <div>
 	<?php echo CHtml::beginForm( null, null, array( 'id' => 'quote-answer-form' ) ); ?>
@@ -23,22 +24,67 @@ $this->breadcrumbs=array(
 		<tr class="">
 			<td class="td-quote">
 				<?php echo CHtml::hiddenField( 'quote_'.$i, $movie->quotes[$rand_qoute]->id ); ?>
-				<?php echo $movie->quotes[ $rand_qoute ]->quote; ?><br /><?php echo $movie->quotes[$rand_qoute]->movie_id; ?>
+				<?php echo $movie->quotes[ $rand_qoute ]->quote; ?>
 			</td>
 			<td>
+				<?php
+					$this->beginWidget('zii.widgets.jui.CJuiDroppable', array(
+						'options'=>array(
+							'drop'=>'js:function( event, ui ) {
+								raw_answer 	= ui.draggable[0].id;
+								movie_id 	= raw_answer.replace( "movie_id_","" );
+								hidden_field = event.target.nextElementSibling;
+								answer_before = jQuery(hidden_field).val();
+								jQuery( hidden_field ).val( movie_id );
+
+								// if there was no answer before, this must be 
+								// a brand new "drop" so we increase the answered
+								// number :)
+								if( answer_before == 0 )
+								{
+									answered_so_far = parseInt( jQuery("#answered_so_far").text() );
+									jQuery( "#answered_so_far" ).text( answered_so_far+1 );
+								}
+							}', //remember put js:
+							)));
+																				?>
 				<div class="movie-droppable"></div>
-				<?php echo CHtml::textField( 'answer_' . $i, 0, array( 'size' => 2 ) ); ?>
+				<?php $this->endWidget(); ?>
+				<?php echo CHtml::hiddenField( 'answer_' . $i, 0 ); ?>
 			</td>
-			<td><?php 
+			<td>
+				<?php 
+				//some draggable object
+				$this->beginWidget('zii.widgets.jui.CJuiDraggable',
+					array(
+						'id'	=> 'movie_id_' . $model->movies_shuffled[$rand_movie_cnt]->id,
+						'htmlOptions'=>array(
+							'style'=>'float:left;width:50px;height:75px;background:#FFEEEE;margin-right:5px;',
+						),
+						'options' => array( 'snap' => false )
+					 ));
 					echo 
 						CHtml::image( 
 							Yii::app()->request->baseUrl . 
 								'/image.php?width=50&height=75&image='. Yii::app()->request->baseUrl . '/' . Yii::app()->params['moviescreenshots'] . $model->movies_shuffled[$rand_movie_cnt]->pic, null, array( 'title' => $model->movies_shuffled[$rand_movie_cnt++]->title ) ) . ' '; 
 
+					$this->endWidget();
+
+				$this->beginWidget('zii.widgets.jui.CJuiDraggable',
+					array(
+						'id'	=> 'movie_id_' . $model->movies_shuffled[$rand_movie_cnt]->id,
+						'htmlOptions'=>array(
+							'style'=>'float:left;width:50px;height:75px;background:#FFEEEE;margin-right:5px;',
+						),
+						'options' => array( 'snap' => false )
+					 ));
+
 					echo 
 						CHtml::image( 
 							Yii::app()->request->baseUrl . 
 								'/image.php?width=50&height=75&image='. Yii::app()->request->baseUrl . '/' . Yii::app()->params['moviescreenshots'] . $model->movies_shuffled[$rand_movie_cnt]->pic, null, array( 'title' => $model->movies_shuffled[$rand_movie_cnt++]->title ) );
+
+					$this->endWidget();
 				?></td>
 		</tr>
 	<?php endfor; ?>
@@ -56,7 +102,7 @@ $this->breadcrumbs=array(
 				jQuery.ajax({
 					'url': gamecontrollerpath + '&' + form_values,
 					'success': function(data){
-						alert( 'heyooo' );
+						alert( 'You answered ' + data + ' wrong!' );
 					},
 					'cache': false
 				});
