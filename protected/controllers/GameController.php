a640d11fe80873695d72050833965ad22a51b65b
Imre Mehesz <imehesz@imehesz-laptop.(none)>
2011-04-02 22:37:28 -0400

    added the logic to the answer checking via AJAX - still lots to do, but a good start

--- protected/controllers/GameController.php
+++ protected/controllers/GameController.php
@@ -9,10 +9,57 @@ class GameController extends Controller
 
 	public function actionPlay()
 	{
-		$level = 4;
+		$level = 2;
 		$game = new Quote;
 		$game->buildGame( $level );
 		$this->render('play', array( 'model' => $game ) );
+	}
+
+	public function actionAjaxCheck()
+	{
+		// we have to collect all the answers and
+		// figure out if they are good or not
+		$get = $_GET;
+
+		// TODO - do this backwards for safety
+		// so the bad answers would start off with
+		// the level number and would go down
+		// with right answers, in that case
+		// if something breaks, just in case
+		// ppl would not go to the next level ....
+		$bad_answers = 0;
+
+		// TODO points needs to multiply by the
+		// level. for example:
+		// on level 1 user gets 1 point/right answer => so 1 point
+		// level 2 user gets 2 points/right answer and so on ... 
+		$points = 0;
+
+		foreach( $get as $key => $val )
+		{
+			if( strstr( $key, 'quote_' ) )
+			{
+				// if we have the quote, we need to get the ID
+				// and the order ID (ie:0,1,2,3) to get the answer associated
+				// with it ...
+				$order_id = (int)str_replace( 'quote_', '', $key );
+				$quote_id = $val;
+
+				$answer_movie_id = $get['answer_'.$order_id];
+
+				if( $answer_movie_id == Quote::model()->findByPk( $quote_id )->movie_id )
+				{
+					$points++; // TODO save this somewhere ...
+				}
+				else
+				{
+					$bad_answers++;
+				}
+			}
+		}
+
+		echo $bad_answers;
+		die();
 	}
 
 	// Uncomment the following methods and override them if needed
--- protected/views/game/play.php
+++ protected/views/game/play.php
@@ -2,10 +2,15 @@
 $this->breadcrumbs=array(
 	'Game'=>array('/game'),
 	'Play',
-);?>
+);
+
+	Yii::app()->clientScript->registerScript( 'gamecontrollerpath', "var gamecontrollerpath='" . Yii::app()->controller->createUrl( '/game/ajaxcheck' ) . "'", CClientScript::POS_HEAD );
+
+?>
 <h1>PLAY</h1>
 
 <div>
+	<?php echo CHtml::beginForm( null, null, array( 'id' => 'quote-answer-form' ) ); ?>
 	<table>
 	<?php $rand_movie_cnt=0; for( $i=0; $i < $model->level ; $i++ ): ?>
 		<?php		
@@ -16,7 +21,14 @@ $this->breadcrumbs=array(
 				}while( ! isset( $movie->quotes[ $rand_qoute ] ) );
 		?>
 		<tr class="">
-			<td style="font-size:20px;width:550px;"><?php echo $movie->quotes[ $rand_qoute ]->quote; ?></td>
+			<td class="td-quote">
+				<?php echo CHtml::hiddenField( 'quote_'.$i, $movie->quotes[$rand_qoute]->id ); ?>
+				<?php echo $movie->quotes[ $rand_qoute ]->quote; ?><br /><?php echo $movie->quotes[$rand_qoute]->movie_id; ?>
+			</td>
+			<td>
+				<div class="movie-droppable"></div>
+				<?php echo CHtml::textField( 'answer_' . $i, 0, array( 'size' => 2 ) ); ?>
+			</td>
 			<td><?php 
 					echo 
 						CHtml::image( 
@@ -31,4 +43,24 @@ $this->breadcrumbs=array(
 		</tr>
 	<?php endfor; ?>
 	</table>
+	<?php echo CHtml::endForm(); ?>
+
+	<?php echo CHtml::button( '  GO  ', array( 'id' => 'gobutton' ) );?>
 </div>
+
+<script language="javascript">
+	jQuery(document).ready( function(){
+		jQuery('body').delegate(
+			'#gobutton', 'click', function(){
+				form_values = jQuery('#quote-answer-form').serialize();
+				jQuery.ajax({
+					'url': gamecontrollerpath + '&' + form_values,
+					'success': function(data){
+						alert( 'heyooo' );
+					},
+					'cache': false
+				});
+			}
+		);
+	});
+</script>
