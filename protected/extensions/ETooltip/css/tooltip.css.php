950a2134be11de782e434af63779b96abf779f4a
Imre Mehesz <imehesz@imehesz-laptop.(none)>
2011-04-04 21:03:27 -0400

    added ETooltip extension http://www.yiiframework.com/extension/etooltip

---
+++ protected/extensions/ETooltip/css/tooltip.css.php
@@ -0,0 +1,11 @@
+<?php
+return ".tooltip {
+        display:none;
+        background:transparent url(".$image.");
+        font-size:14px;
+        height:45px;
+        width:105px;
+        padding: 10px 5px 5px 5px;
+        color:#fff; 
+		text-align:center;
+}";
