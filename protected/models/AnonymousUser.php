1353205d6edd3325b5c69eaf9d20aa41515d6932
Imre Mehesz <imehesz@imehesz-BT456AAR-ABA-P6627C.(none)>
2011-04-10 16:06:59 -0400

    added anonymous user model to handle anonymous users

---
+++ protected/models/AnonymousUser.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * This is the model class for table "anonymous_user".
+ *
+ * The followings are the available columns in table 'anonymous_user':
+ * @property integer $id
+ * @property string $name
+ * @property string $session_id
+ * @property integer $level
+ * @property integer $score
+ * @property integer $created
+ * @property integer $updated
+ */
+class AnonymousUser extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @return AnonymousUser the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'anonymous_user';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('session_id', 'required'),
+			array('level, score, created, updated', 'numerical', 'integerOnly'=>true),
+			array('name', 'length', 'max'=>255),
+			array('session_id', 'length', 'max'=>100),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('id, name, session_id, level, score, created, updated', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'name' => 'Name',
+			'session_id' => 'Session',
+			'level' => 'Level',
+			'score' => 'Score',
+			'created' => 'Created',
+			'updated' => 'Updated',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('name',$this->name,true);
+		$criteria->compare('session_id',$this->session_id,true);
+		$criteria->compare('level',$this->level);
+		$criteria->compare('score',$this->score);
+		$criteria->compare('created',$this->created);
+		$criteria->compare('updated',$this->updated);
+
+		return new CActiveDataProvider(get_class($this), array(
+			'criteria'=>$criteria,
+		));
+	}
+
+    public function beforeValidate()
+    {
+        parent::beforeValidate();
+        $now = time();
+
+        if( $this->isNewRecord )
+        {
+            $this->created      = $now;
+            $this->level        = 1;
+            $this->score        = 0;
+            $this->session_id   = Yii::app()->session->sessionID;
+            $this->name         = strtoupper( substr( $this->session_id,0,6 ) );
+        }
+
+        $this->updated = $now;
+
+        return true;
+    }
+
+    public function updateLevelScore( $newlevel, $newscore )
+    {
+        $this->level = $newlevel;
+        $this->score = $newscore;
+        $this->updated = time();
+
+        $this->update();
+    }
+}
