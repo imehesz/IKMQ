8d0893a720223db5cde4d6b08c87b67443e270ef
Imre Mehesz <imehesz@imehesz-laptop.(none)>
2011-04-20 13:51:31 -0400

    added google tracking code and > and || to play and pause

--- themes/ikmq/views/layouts/main.php
+++ themes/ikmq/views/layouts/main.php
@@ -14,6 +14,21 @@
 			<link rel="stylesheet" type="text/css" href="ie67hacks.css" />
 		<![endif]-->
         <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/ikmq.js"></script>
+		<?php if( ! YII_DEBUG  ) : ?>
+				<script type="text/javascript">
+
+				  var _gaq = _gaq || [];
+				  _gaq.push(['_setAccount', 'UA-5417349-8']);
+				  _gaq.push(['_trackPageview']);
+
+				  (function() {
+					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+				  })();
+
+				</script>
+		<?php endif; ?>
 </head>
 <body>
 <?php if( preg_match( '/MSIE/i',$_SERVER['HTTP_USER_AGENT'] ) ) : ?>
@@ -49,8 +64,8 @@
 		<?php 
             $play_item = 
                 ( Yii::app()->controller->id == 'game' && Yii::app()->controller->action->id == 'play' ) ?
-                array( 'label' => 'Pause', 'url' => array( '/game/pause' ) ) :
-                array( 'label' => 'Play', 'url' => array( '/game/play' ) );
+                array( 'label' => 'Pause ||', 'url' => array( '/game/pause' ) ) :
+                array( 'label' => 'Play >', 'url' => array( '/game/play' ) );
 
             $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
