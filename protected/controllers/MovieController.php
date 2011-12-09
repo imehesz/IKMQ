30dffd61c4fe01b30bd0fa3ee078de6d474308ce
Imre Mehesz <imehesz@imehesz-laptop.(none)>
2011-12-09 13:27:46 -0500

    updated movie and quote controllers to don't let anonymous listings

--- protected/controllers/MovieController.php
+++ protected/controllers/MovieController.php
@@ -27,11 +27,11 @@ class MovieController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view', 'ajaxmovielist' ),
+				'actions'=>array('ajaxmovielist' ),
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
+				'actions'=>array( 'index', 'view', 'create','update'),
 				'users'=>array('@'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
